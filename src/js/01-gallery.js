6cbc72d93d0a2825626ab8d53fc5995df6af382e
JulsZhovanyk <julia.zhovanyk@gmail.com>
2022-12-20 16:34:18 +0200

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+
+const createGallery = items => {
+  const createGalleryEl = items
+    .map(
+      ({ original, preview, description }) =>
+        `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"></a>`
+    )
+    .join('');
+
+  gallery.innerHTML = createGalleryEl;
+};
+
+createGallery(galleryItems);
+
+function onImgClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+
+const instance = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+gallery.addEventListener('click', onImgClick);
 
 console.log(galleryItems);

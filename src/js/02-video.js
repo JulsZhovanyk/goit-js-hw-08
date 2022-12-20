e516d40cccca6881fb5ba6beb87e6eb071cfbb5b
JulsZhovanyk <julia.zhovanyk@gmail.com>
2022-12-20 17:29:40 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const time = 'videoplayer-current-time';
 
+const playHandle = function (data) {
+  localStorage.getItem(time, JSON.stringify(data));
+};
+player.on('timeUpdate', throttle(playHandle, 1000));
+
+try {
+  const dataParced = JSON.parse(localStorage.getItem(time));
+  player
+    .setCurrentTime(parceData.seconds)
+    .then(function (sec) {
+      sec = dataParced.sec;
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          dataParced.sec < 0 || dataParced.sec > dataParced.duration;
+          break;
+      }
+    });
+} catch (error) {
+  error.name;
+  error.message;
+}

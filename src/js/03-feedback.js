7c7c406c06a3679de5615f07f99b46b55ddbdb2d
JulsZhovanyk <julia.zhovanyk@gmail.com>
2022-12-20 18:09:41 +0200

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+const formRef = document.querySelector('.feedback-form');
+const throttle = require('lodash.throttle');
 
+formRef.addEventListener('input', throttle(onTextareaInput, 500));
+formRef.addEventListener('submit', onFormSubmit);
+
+addInTextarea();
+
+const formData = {};
+
+function onTextareaInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  if (
+    e.currentTarget.elements.email.value === '' ||
+    e.currentTarget.elements.message.value === ''
+  ) {
+    alert('Мають бути заповнені всі поля!');
+    return;
+  }
+  e.currentTarget.reset();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.removeItem('feedback-form-state');
+}
+
+function addInTextarea() {
+  const savedData = localStorage.getItem('feedback-form-state');
+  try {
+    const dataArr = JSON.parse(savedData);
+    if (dataArr.email) {
+      formRef.elements.email.value = dataArr.email;
+    }
+
+    if (dataArr.message) {
+      formRef.elements.message.value = dataArr.message;
+    }
+  } catch (error) {
+    error.name;
+    error.message;
+  }
+}

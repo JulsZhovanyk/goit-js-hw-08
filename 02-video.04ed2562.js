a05a41f9cfa6304ec2360a25a7e30a7350c30f66
JulsZhovanyk <109437050+JulsZhovanyk@users.noreply.github.com>
2022-12-20 15:37:49 +0000

    Deploying to gh-pages from @ JulsZhovanyk/goit-js-hw-08@44296238aa50a85ebbca99b34e872e6719e08c24 🚀

--- 02-video.04ed2562.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("timeUpdate",throttle((function(e){localStorage.getItem("videoplayer-current-time",JSON.stringify(e))}),1e3));try{const e=JSON.parse(localStorage.getItem("videoplayer-current-time"));t.setCurrentTime(parceData.seconds).then((function(t){e.sec})).catch((function(t){if("RangeError"===t.name)e.sec<0||(e.sec,e.duration)}))}catch(e){e.name,e.message}
-//# sourceMappingURL=02-video.04ed2562.js.map

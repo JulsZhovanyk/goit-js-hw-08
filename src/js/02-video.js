0c2ecb5e40d90b72fe02c48199e0f370c2a10b0b
JulsZhovanyk <julia.zhovanyk@gmail.com>
2022-12-20 17:46:07 +0200

    again task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,6 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 const throttle = require('lodash.throttle');
